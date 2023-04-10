5521e1afcb4c18461637eb7f8f011b516e5dba48
roger-rab-bit <ruggero-dolzi@virgilio.it>
2023-04-11 01:29:47 +0200

    Bug fix

--- ft_printf.c
+++ ft_printf.c
@@ -26,6 +26,94 @@
 
 // Obs: Your function must not have memory leaks. Moulinette will test that.
 
+#include <stdarg.h>
+#include <unistd.h>
+
+void ft_printchar(char c, int *sum)
+{
+    write(1, &c, 1);
+    (*sum)++;
+}
+
+void ft_printstr(char *str, int *sum)
+{
+    int i;
+
+    i = 0;
+    while (str[i])
+    {
+        ft_printchar(str[i++], sum);
+    }
+}
+
+void ft_printnbr(int nbr, int *sum)
+{
+    if (nbr == -2147483648)
+    {
+        ft_printstr("-2147483648", sum);
+        return;
+    }
+    if (nbr < 0)
+    {
+        ft_printchar('-', sum);
+        nbr *= -1;
+    }
+    if (nbr > 9)
+        ft_printnbr(nbr / 10, sum);
+    ft_printchar((nbr % 10) + '0', sum);
+}
+
+void ft_printhex(unsigned int nbr, int *sum)
+{
+    char *base;
+    base = "0123456789abcdef";
+    if (nbr > 15)
+        ft_printhex(nbr / 16, sum);
+    ft_printchar(base[nbr % 16], sum);
+}
+
+void ft_checker(char c, va_list *va, int *sum)
+{
+    if (c == 's')
+    {
+        ft_printstr(va_arg(*va, char *), sum);
+    }
+    else if (c == 'd')
+    {
+        ft_printnbr(va_arg(*va, int), sum);
+    }
+    else if (c == 'x')
+    {
+        ft_printhex(va_arg(*va, unsigned int), sum);
+    }
+}
+
 int ft_printf(const char *str, ...)
 {
+    int i;
+    int sum;
+    va_list va;
+
+    i = 0;
+    sum = 0;
+    va_start(va, str);
+    while (str[i])
+    {
+        if (str[i] == '%')
+        {
+            ft_checker(str[++i], &va, &sum);
+            i++;
+        }
+        else
+            ft_printchar(str[i++], &sum);
+    }
+    va_end(va);
+    return (sum);
+}
+
+int main()
+{
+    // ft_printf("%s\n", "toto");
+    // ft_printf("Magic %s is %d", "number", 42);
+    ft_printf("Hexadecimal for %d is %x\n", 42, 42);
 }
